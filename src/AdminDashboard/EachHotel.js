029ca666da9ff0e55c7dbecc64a6543694b23390
ramatronics1 <shrihegde661@gmail.com>
2024-04-06 13:00:44 +0530

    EachHotel Changes

--- src/AdminDashboard/EachHotel.js
+++ src/AdminDashboard/EachHotel.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 import DisplayDishes from "./DisplayDishes";
-import styles from './EachHotel.module.css'
-import AddtoCart from "../ClientDashBoard/addtoCart";
+import styles from './EachHotel.module.css';
 
-const EachHotel = ({show,dish,setDish,handleChange,handleClick}) => {
-  const [data, setData] = useState(null);
+const EachHotel = () => {
+  const [hotelData, setData] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const { id } = useParams();
@@ -38,35 +37,46 @@ const EachHotel = ({show,dish,setDish,handleChange,handleClick}) => {
   }, [id]);
 
   return (
-    <div>
-      {show}
-      {show ? (
-        <AddtoCart dish={dish} setDish={setDish} handleChange={handleChange}  handleClick={handleClick}/>
-      ) : (
-        <div>
-          <h1>Hotel Details</h1>
-          {data ? (
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: '5px' }}>
-              <p>Hotel ID: <u>{data.imageUrl[0].url}</u></p>
-  
-              <div className={styles.displayDiv}>
-                {!isLoggedIn && (
-                  <div>
-                    <p>Already a Customer/Join Now!</p>{" "}
-                    <Link className={styles.Login} to={{ pathname: `/adminSignup/${data._id}` }}>Sign Up</Link>{" "}
-                    <Link className={styles.Signup} to={{ pathname: `/adminLogin/${data._id}` }}>Login</Link>
-                  </div>
-                )}
+    <div style={{ backgroundColor: '#feebda' }}>
+      <h1>Hotel Details</h1>
+      {hotelData ? (
+        <div className={styles.displayDiv}>
+          <div>
+            <p><strong>Hotel Name:</strong> {hotelData.name}</p>
+            <p><strong>Description:</strong> {hotelData.description}</p>
+            <p><strong>Phone:</strong> {hotelData.phone}</p>
+            <p><strong>Email:</strong> {hotelData.email}</p>
+            <p><strong>Images:</strong></p>
+            <ul>
+              {hotelData.imageUrl.map((img) => (
+                <li key={img._id}>
+                  <img src={img.url} alt="Hotel" />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            {!isLoggedIn && (
+              <div>
+                <p>Already a Customer/Join Now!</p>{" "}
+                <Link to={{ pathname: `/adminSignup/${hotelData._id}` }} className={styles.Signup}>Sign Up</Link>{" "}
+                <Link to={{ pathname: `/adminLogin/${hotelData._id}` }} className={styles.Login}>Login</Link>
               </div>
-            </div>
-          ) : (
-            <p>Loading...</p>
-          )}
+            )}
+            {isLoggedIn && (
+              <div>
+                <Link to={{ pathname: `/DisplayDishes/${hotelData._id}` }} className={styles.displayDishes}>
+                  DisplayDishes <span>&rarr;</span>
+                </Link>
+              </div>
+            )}
+          </div>
         </div>
+      ) : (
+        <p>Loading...</p>
       )}
     </div>
-  );
-  
+  ); 
 };
 
 export default EachHotel;
